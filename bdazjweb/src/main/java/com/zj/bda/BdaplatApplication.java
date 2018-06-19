c0d4da4b7da9f09f5b3e7c78be9d33233673c3c9
dongguabai <weiminbron@163.com>
2018-06-19 15:32:53 +0800

    增加异步调用、定时任务支持注解

--- bdazjweb/src/main/java/com/zj/bda/BdaplatApplication.java
+++ bdazjweb/src/main/java/com/zj/bda/BdaplatApplication.java
@@ -4,12 +4,16 @@ import com.zj.bda.common.listener.InitApplicationContextLoaderListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.zj.bda"})
 @MapperScan(basePackages = "com.zj.bda.persistence.mapper")
 //@ServletComponentScan
+@EnableAsync  //class @Component   method @Async
+@EnableScheduling
 public class BdaplatApplication {
 
     public static void main(String[] args) {
