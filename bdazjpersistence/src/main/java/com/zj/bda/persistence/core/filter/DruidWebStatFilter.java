63f705ccb17191b2446f99ca675b96d5433c1041
dongguabai <weiminbron@163.com>
2018-06-19 21:33:56 +0800

    修改druid配置

--- bdazjpersistence/src/main/java/com/zj/bda/persistence/core/filter/DruidWebStatFilter.java
+++ bdazjpersistence/src/main/java/com/zj/bda/persistence/core/filter/DruidWebStatFilter.java
@@ -2,13 +2,10 @@ package com.zj.bda.persistence.core.filter;
 
 import com.alibaba.druid.support.http.WebStatFilter;
 
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.annotation.WebInitParam;
-
 /**
  * Created by Dongguabai on 2018-06-19 0:12
  */
-@WebFilter(filterName = "druidWebStatFilter", urlPatterns = "/*", initParams = { @WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*") })
+//@WebFilter(filterName = "druidWebStatFilter", urlPatterns = "/*", initParams = { @WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*") })
 public class DruidWebStatFilter extends WebStatFilter {
 
 }
--- bdazjpersistence/src/main/java/com/zj/bda/persistence/core/servlet/DruidStatViewServlet.java
+++ bdazjpersistence/src/main/java/com/zj/bda/persistence/core/servlet/DruidStatViewServlet.java
@@ -2,13 +2,10 @@ package com.zj.bda.persistence.core.servlet;
 
 import com.alibaba.druid.support.http.StatViewServlet;
 
-import javax.servlet.annotation.WebInitParam;
-import javax.servlet.annotation.WebServlet;
-
 /**
  * Created by Dongguabai on 2018-06-19 0:12
  */
-@WebServlet(urlPatterns = { "/druid/*" }, initParams = { @WebInitParam(name = "loginUsername", value = "bda"), @WebInitParam(name = "loginPassword", value = "bda") })
+//@WebServlet(urlPatterns = { "/druid/*" }, initParams = { @WebInitParam(name = "loginUsername", value = "bda"), @WebInitParam(name = "loginPassword", value = "bda") })
 public class DruidStatViewServlet extends StatViewServlet {
 
 	private static final long serialVersionUID = 1L;
