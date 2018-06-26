212fc9432e9ffaff10cd086cd825a5821a1f3352
dongguabai <weiminbron@163.com>
2018-06-26 12:11:59 +0800

    增加调度模块，增加部分内容，方便项目以后可以在jar和war切换部署

--- bdazjweb/src/main/java/com/zj/bda/BdaplatApplication.java
+++ bdazjweb/src/main/java/com/zj/bda/BdaplatApplication.java
@@ -14,7 +14,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 //@ServletComponentScan
 @EnableAsync  //class @Component   method @Async
 @EnableScheduling
-//@EnableAspectJAutoProxy(exposeProxy = true)
+//@EnableAspectJAutoProxy(exposeProxy = true)  开启cglib代理，并且AopContext可以获取代理对象
 public class BdaplatApplication {
 
     public static void main(String[] args) {
@@ -26,3 +26,22 @@ public class BdaplatApplication {
        // SpringApplication.run(BdaplatApplication.class, args);
     }
 }
+//war用
+/*
+public class BdaplatApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+
+        SpringApplication bdaSpringApplication = new SpringApplication(BdaplatApplication.class);
+        bdaSpringApplication.addListeners(new InitApplicationContextLoaderListener());
+        bdaSpringApplication.run(args);
+
+        // SpringApplication.run(BdaplatApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // bulider  main类
+        return builder.sources(BdaplatApplication.class);
+    }
+}*/
