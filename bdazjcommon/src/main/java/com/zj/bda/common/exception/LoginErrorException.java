5fd2e22b410a056157778a68b520c8c56c4869bc
dongguabai <weiminbron@163.com>
2018-07-13 10:50:25 +0800

    增加登陆失败Exception

---
+++ bdazjcommon/src/main/java/com/zj/bda/common/exception/LoginErrorException.java
@@ -0,0 +1,30 @@
+package com.zj.bda.common.exception;
+
+import lombok.NoArgsConstructor;
+
+/**
+ * 登陆失败
+ * @author Dongguabai
+ * @date 2018-07-01 13:30
+ */
+@NoArgsConstructor
+public class LoginErrorException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public LoginErrorException(String message) {
+		super(message);
+	}
+
+	public LoginErrorException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public LoginErrorException(Throwable cause) {
+		super(cause);
+	}
+
+	public LoginErrorException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+}
