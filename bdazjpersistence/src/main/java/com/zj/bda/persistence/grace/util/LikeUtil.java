977104d361f0e5e34e90b49045797fb377ef39ae
dongguabai <weiminbron@163.com>
2018-07-20 15:55:41 +0800

    Like工具类参数不能为null

--- bdazjpersistence/src/main/java/com/zj/bda/persistence/grace/util/LikeUtil.java
+++ bdazjpersistence/src/main/java/com/zj/bda/persistence/grace/util/LikeUtil.java
@@ -1,6 +1,7 @@
 package com.zj.bda.persistence.grace.util;
 
 import com.google.common.base.Joiner;
+import com.zj.bda.common.exception.InvalidParameterException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -13,15 +14,24 @@ import lombok.NoArgsConstructor;
 public class LikeUtil {
 
     public static String leftLike(Object value){
-        return value==null?"":Joiner.on("").join("%",value);
+        if (value==null){
+            throw new InvalidParameterException("模糊查询参数不能为空！");
+        }
+        return Joiner.on("").join("%",value);
     }
 
     public static String rightLike(Object value){
-        return value==null?"":Joiner.on("").join(value,"%");
+        if (value==null){
+            throw new InvalidParameterException("模糊查询参数不能为空！");
+        }
+        return Joiner.on("").join(value,"%");
     }
 
     public static String leftAndRightLike(Object value){
-        return value==null?"":Joiner.on("").join("%",value,"%");
+        if (value==null){
+            throw new InvalidParameterException("模糊查询参数不能为空！");
+        }
+        return Joiner.on("").join("%",value,"%");
     }
 
 }
