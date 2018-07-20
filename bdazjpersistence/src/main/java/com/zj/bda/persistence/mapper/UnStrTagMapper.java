f9d5abda8e8a2e4b7624048634e925946c6d7605
dongguabai <weiminbron@163.com>
2018-07-20 09:49:23 +0800

    关闭mybatis二级缓存

--- bdazjpersistence/src/main/java/com/zj/bda/persistence/mapper/UnStrTagMapper.java
+++ bdazjpersistence/src/main/java/com/zj/bda/persistence/mapper/UnStrTagMapper.java
@@ -1,14 +1,12 @@
 package com.zj.bda.persistence.mapper;
 
-import com.zj.bda.persistence.grace.BaseMapper;
 import com.zj.bda.persistence.entity.UnStrTag;
-import org.apache.ibatis.annotations.CacheNamespace;
+import com.zj.bda.persistence.grace.BaseMapper;
 
 /**
  * @author Dongguabai
  * @date 2018-07-19 15:12
  */
-@CacheNamespace
 public interface UnStrTagMapper extends BaseMapper<UnStrTag> {
 
 }
