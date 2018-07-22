c26de363512e7bf337853cb3ae6f88cf1298c1fa
dongguabai <weiminbron@163.com>
2018-07-22 22:21:39 +0800

    WebUtil增加判断是否是ajax方法

--- bdazjcommon/src/main/java/com/zj/bda/common/util/WebUtil.java
+++ bdazjcommon/src/main/java/com/zj/bda/common/util/WebUtil.java
@@ -2,9 +2,12 @@ package com.zj.bda.common.util;
 
 import com.zj.bda.common.web.helper.ResponseHelper;
 import com.zj.bda.common.web.vo.ResponseVO;
+import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -12,8 +15,17 @@ import java.io.IOException;
  * @author Dongguabai
  * @date 2018-07-05 20:24
  */
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class WebUtil {
+
+    public static boolean isAjax(HttpServletRequest request){
+        boolean isAjaxRequest = false;
+        if(!StringUtils.isBlank(request.getHeader("x-requested-with")) && request.getHeader("x-requested-with").equals("XMLHttpRequest")){
+            isAjaxRequest = true;
+        }
+        return isAjaxRequest;
+    }
+
 
     public static void responseErrorJson(HttpServletResponse response,Object resData,HttpStatus httpStatus,String message,Integer errorCode) throws IOException {
         response.setStatus(httpStatus.value());
