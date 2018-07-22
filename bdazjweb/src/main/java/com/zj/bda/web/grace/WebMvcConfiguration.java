4461df8dc84575bd05c5101b94293b74edd44a28
dongguabai <weiminbron@163.com>
2018-07-22 22:52:00 +0800

    xss过滤器统一由web端注册使用

--- bdazjweb/src/main/java/com/zj/bda/web/grace/WebMvcConfiguration.java
+++ bdazjweb/src/main/java/com/zj/bda/web/grace/WebMvcConfiguration.java
@@ -3,12 +3,15 @@ package com.zj.bda.web.grace;
 import com.google.common.collect.Lists;
 import com.zj.bda.web.grace.converter.DateConverter;
 import com.zj.bda.web.grace.interceptor.HttpInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import wm.dgb.security.support.safe.xss.XssFilter;
 
 /**
  * @author Dongguabai
@@ -32,6 +35,19 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new HttpInterceptor()).addPathPatterns("/**").excludePathPatterns(Lists.newArrayList("/assets/**","/**/*.html","/druid/**"));
         //registry.addInterceptor(new HttpInterceptor()).addPathPatterns("/**").excludePathPatterns(Lists.newArrayList("/css/**", "/img/**", "/js/**","/**/*.html"));
+    }
+
+    /**
+     * xss过滤拦截器
+     */
+    @Bean
+    public FilterRegistrationBean xssFilterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new XssFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.setEnabled(true);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
     }
 
 }
