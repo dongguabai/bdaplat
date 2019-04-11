52ba8ee0830c42af243fc001f01ff26c11cf681b
dongguabai <weiminbronn@163.com>
2019-04-11 09:49:47 +0800

    去除多余依赖

--- bdazjweb/src/main/java/com/zj/bda/BdaplatApplication.java
+++ bdazjweb/src/main/java/com/zj/bda/BdaplatApplication.java
@@ -4,7 +4,6 @@ import com.zj.bda.common.init.listener.InitApplicationContextLoaderListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
